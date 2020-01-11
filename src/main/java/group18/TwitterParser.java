49bc8628c8856db7cabbceebd97cb2521524cf8a
ostritze <oliver.stritzel@googlemail.com>
2020-01-11 20:10:49 +0100

    twitter runs

--- src/main/java/group18/TwitterParser.java
+++ src/main/java/group18/TwitterParser.java
@@ -1,9 +1,6 @@
 package group18;
 
-import org.eclipse.rdf4j.model.IRI;
-import org.eclipse.rdf4j.model.Model;
-import org.eclipse.rdf4j.model.Value;
-import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.*;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
@@ -13,26 +10,13 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import org.noggit.JSONUtil;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
 import java.util.Random;
 
-/**
- *	?tweet a movies:Tweet ;
- * 		movies:__-_id ?id ;
- * 		movies:__-_text ?text ;
- * 		movies:__-_lang ?lang ;
- * 		movies:__-_source ?source ;
- * 		movies:__-_created_at ?created_at ;
- * 		movies:__-_retweet_count ?retweet_count ;
- * 		movies:__-_favorite_count ?favorite_count ;
- * 		movies:__-_in_reply_to_user_id ?in_reply_to_user_id ;
- * 		movies:__-_in_reply_to_status_id ?in_reply_to_status_id ;
- * 		movies:__-_in_reply_to_user_id_str ?in_reply_to_user_id_str ;
- * 		movies:__-_in_reply_to_screen_name ?in_reply_to_screen_name ;
- * 		movies:__-_user_-_id ?user_id ;
- * 		movies:__-_user_-_screen_name ?user_name .
- * */
 
 public class TwitterParser{
 
@@ -40,7 +24,7 @@ public class TwitterParser{
 
         JSONArray ret = new JSONArray();
         JSONParser parser = new JSONParser();
-        Object obj = parser.parse(new FileReader("twitter/all.json"));
+        Object obj = parser.parse(new FileReader(getClass().getClassLoader().getResource("twitter/all.json").getPath()));
         JSONArray fullJSON = new JSONArray(obj.toString());
 
         for (int i = 0; i < fullJSON.length(); i++) {
@@ -94,20 +78,25 @@ public class TwitterParser{
             return "";
     }
 
-    public void initialize(Repository repo, Model model) {
+    public void initialize(Repository repo, Map<String, IRI> iris) {
         JSONArray tweets;
         ValueFactory valueFactory = repo.getValueFactory();
-        Value commentType = model.objects().stream().filter(o -> o.stringValue().contains("Post")).findFirst().get();
+        //Value commentType = model.objects().stream().filter(o -> o.stringValue().contains("Post")).findFirst().get();
         int counter = 0;
 
         try {
             tweets = parser();
-            System.out.println(tweets.toString());
             RepositoryConnection conn = repo.getConnection();
             for (int i = 0; i < tweets.length(); i++) {
-                IRI commentIri = valueFactory.createIRI(Util.NS, "post" + (counter+i));
+                JSONObject thisTweet = tweets.getJSONObject(i);
+                IRI tweetIri = valueFactory.createIRI(Util.NS, "twitter/post#" + (counter+i));
+                System.out.println(iris.get("Tweet"));
+                conn.add(tweetIri, RDF.TYPE, iris.get("Tweet"));
 
-                conn.add(commentIri, RDF.TYPE, commentType);
+                conn.add(tweetIri,
+                        iris.get("createdBy"),
+                        valueFactory.createLiteral( thisTweet.getString("user_name") ));
+
 
             }
 
